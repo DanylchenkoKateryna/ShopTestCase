68e7b164be071c51de7ba435495ae957f8cd4dac
Kateryna Danylchenko <kdanilchenko265@gmail.com>
2024-04-11 11:51:37 +0300

    implement ShopContext class

---
+++ ShopTestCase/ShopTestCase/Data/ShopContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTestCase.Entities;
+using System.Reflection.Emit;
+
+namespace ShopTestCase.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<OrderProduct>()
+                .Property(p => p.TotalPrice)
+                .HasColumnType("decimal(18,2)");
+        }
+
+    }
+}
--- ShopTestCase/ShopTestCase/Program.cs
+++ ShopTestCase/ShopTestCase/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ShopTestCase.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ShopContext>(opts =>
+ opts.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
