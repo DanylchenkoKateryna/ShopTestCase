6cd39d375ef5fcd5efe0a38ace36d7eeae870bed
Kateryna Danylchenko <kdanilchenko265@gmail.com>
2024-04-11 11:28:40 +0300

    add entities

---
+++ ShopTestCase/ShopTestCase/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopTestCase.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CustomerFullName { get; set; }
+        public string CustomerPhone { get; set; }
+        public List<OrderProduct> OrderProducts { get; set; }
+    }
+}
---
+++ ShopTestCase/ShopTestCase/Entities/OrderProduct.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopTestCase.Entities
+{
+    public class OrderProduct
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int OrderId { get; set; }
+        public int Amount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+}
